d7a144222ffea330dab6a546e811a82ed2c77a79
Herfando <herfando_91@yahoo.com>
2025-12-04 10:54:20 +0700

    finished navbarUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './index.css';
 // import Login from '@/pages/01_login';
 // import Register from './pages/02_register';
-import NavbarGuest from './components/layout/00_NavbarGuest';
+// import NavbarGuest from './components/layout/00_NavbarGuest';
+import NavbarUser from './components/layout/00_NavbarUser';
 
 function App() {
   return (
     <>
-      <NavbarGuest />
+      {/* <NavbarGuest /> */}
+      <NavbarUser />
       {/* <Login /> */}
       {/* <Register /> */}
     </>
