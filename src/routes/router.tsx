db9d5150a4a642b37658a373f643fe9e1f90c947
Herfando <herfando_91@yahoo.com>
2025-12-07 12:48:26 +0700

    edit router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,6 +4,8 @@ import Home from '@/pages/03_home';
 import Login from '@/pages/01_login';
 import Register from '@/pages/02_register';
 import NavbarGuest from '@/components/layout/01_navbarGuest';
+import { Toaster } from 'react-hot-toast';
+import NavbarUser from '@/components/layout/02_navbarUser';
 
 // Before Auth
 const BeforeAuthLayout = () => (
@@ -17,12 +19,46 @@ const BeforeAuthLayout = () => (
   </div>
 );
 
+// Auth
+const AuthLayout = () => (
+  <div>
+    <Toaster position='top-right' />
+    <main>
+      <Outlet />
+    </main>
+  </div>
+);
+
+// After Auth
+const AfterAuthLayout = () => {
+  return (
+    <div>
+      <NavbarUser />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 export default function Router() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      {/* Before Auth */}
+      <Route element={<BeforeAuthLayout />}>
+        <Route path='/' element={<Home />} />
+      </Route>
+
+      {/* Auth */}
+      <Route element={<AuthLayout />}>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Route>
+
+      {/* After Auth */}
+      <Route element={<AfterAuthLayout />}>
+        <Route path='/home' element={<Home />} />
+      </Route>
     </Routes>
   );
 }
